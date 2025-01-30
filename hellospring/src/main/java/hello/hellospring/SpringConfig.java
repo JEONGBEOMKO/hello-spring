0e98eca0b98639e61f3398e0a3d7aec5fa1aa1dd
JEONGBEOM KO <beom9807@gmail.com>
2025-01-30 17:29:05 +0900

    AOP 적용
    TimeTraceAop를 작성하여 시간을 측정하는 로직을 별도의 공통 로직으로 분리하였다.
    - 핵심 비즈니스 로직을 깔끔하게 유지할 수 있다.

--- hellospring/src/main/java/hello/hellospring/SpringConfig.java
+++ hellospring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,11 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
-import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
@@ -24,8 +22,9 @@ public class SpringConfig {
         return new MemberService(memberRepository);
     }
 
+
 //    @Bean
-//    public MemberRepository memberRepository(){
+//     public MemberRepository memberRepository(){
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
